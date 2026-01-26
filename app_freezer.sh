1c98f9754e912ca72af49f427513bee39591f482
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-26 08:31:20 +0700

    Update app_freezer.sh

--- app_freezer.sh
+++ app_freezer.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
-clear
-read -p "Package name: " pkg
-su -c "pm disable-user --user 0 $pkg"
-echo "[✓] App frozen"
-read; bash main.sh
+#!/data/data/com.termux/files/usr/bin/bash
+. ./modules/_common.sh
+
+MODUL="Freeze App"
+echo "[*] Menjalankan $MODUL"
+loading
+
+read -p "Nama package: " p
+need_root
+tsu -c "pm disable-user --user 0 $p"
+
+back
