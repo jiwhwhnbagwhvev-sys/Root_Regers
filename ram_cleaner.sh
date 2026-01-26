9d1d6b73a31b1400215263ba1d08049bb0296e7f
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-26 08:27:01 +0700

    Update ram_cleaner.sh

--- ram_cleaner.sh
+++ ram_cleaner.sh
@@ -1,5 +1,13 @@
-#!/system/bin/sh
-su -c "sync; echo 3 > /proc/sys/vm/drop_caches"
-echo "[✓] RAM CLEANED"
-read -p "Enter..."
-exec ../main.sh
+
+#!/data/data/com.termux/files/usr/bin/bash
+. ./modules/_common.sh
+
+MODUL="RAM Cleaner"
+echo "[*] Menjalankan $MODUL"
+loading
+
+need_root
+tsu -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+free -h
+
+back
