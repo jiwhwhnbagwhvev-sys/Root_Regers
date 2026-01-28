fee328bdecfd0c4dff6fbfb62813148524894b8b
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-01-28 22:10:10 +0700

    Update dark_coding_lab.sh

--- modules/dark_coding_lab.sh
+++ modules/dark_coding_lab.sh
@@ -1,24 +1,24 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # DARK CODING LAB MODULE
 
-ENGINE="engine/dark_coding_lab.py"
+ENGINE="engine/dark_coding_lab_engine.py"
 
 clear
 echo -e "\e[1;31m"
-echo "╔══════════════════════════════╗"
-echo "║     DARK CODING LAB MODULE   ║"
-echo "╚══════════════════════════════╝"
+echo "╔════════════════════════════════════╗"
+echo "║        DARK CODING LAB MODULE      ║"
+echo "╚════════════════════════════════════╝"
 echo -e "\e[0m"
 
 if ! command -v python >/dev/null; then
-    echo "[*] Install python..."
-    pkg install python -y
+  echo "[*] Install python..."
+  pkg install python -y
 fi
 
 if [ ! -f "$ENGINE" ]; then
-    echo "[!] Engine tidak ditemukan!"
-    read -rp "ENTER..."
-    exit 1
+  echo "[!] Engine tidak ditemukan!"
+  read -rp "ENTER..."
+  exit 1
 fi
 
 python "$ENGINE"
